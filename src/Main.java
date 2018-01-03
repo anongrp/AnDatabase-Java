064cb87f1e8e7d2047c29ff005c3643670fc3b55
Gaurav8871 <gauravthakur40127@gmail.com>
2018-01-03 09:22:34 +0530

    Table.java Changed

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import anon.database.Database;
 import anon.database.Table;
 import anon.database.connect.Connection;
 
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if (Connection.connect("Tublime Text")){
             System.out.println("Connection Ok");
             Database mainDB = new Database("mainDB");
             Database primaryDB = new Database("Primary");
-            Table studentTB = new Table("MainTB",10,mainDB);
+            Table table=new Table("table3",2,primaryDB);
+            String col[]={"name","Email","Mobile NO."};
+            table.addColumn(col);
+            String row1[]={"gaurav","gauravthakur40127@gmail.com","   8871568598"};
+            table.addRow(row1);
+
         }else{
             System.out.println("Not Ok");
         }
--- src/anon/database/Table.java
+++ src/anon/database/Table.java
@@ -1,30 +1,62 @@
 package anon.database;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Table {
     private String tbName = "tb-temp";
-    private Integer column;
     private File tbDir;
-    private boolean status=false;
+    private boolean status = false;
+    private int countForColumn = 0;
+    private int countForRow = 0;
+    private Integer Column = 0;
 
     // Default Constructor
-    public Table(){}
+    public Table(String mainTB2, Database mainDB) {
+    }
 
     // Constructor
-    public Table(String tbName,Integer column,Database database){
-        createTable(tbName,column,database);
+    public Table(String tbName, Integer Column, Database database) throws IOException {
+        createTable(tbName, database);
+        this.Column = Column;
     }
 
     /* Method For Create Table */
-    public boolean createTable(String tbName,Integer column,Database database) {
-        this.tbName = "tb-"+tbName;
-        this.column = column;
-        tbDir = new  File(database.dbDir.getAbsolutePath()+"\\"+this.tbName);
+    private boolean createTable(String tbName, Database database) throws IOException {
+        this.tbName = "tb-" + tbName;
+        tbDir = new File(database.dbDir.getAbsolutePath() + "\\" + this.tbName + ".andb");
 
-        if (database.dbStatus){
-            tbDir.mkdir();
-            status=true;
+        if (database.dbStatus) {
+            tbDir.createNewFile();
+            status = true;
         }
         return false;
+    }
+
+    public void addColumn(String ColumnName[]) throws IOException {
+        countForColumn++;
+        if (countForColumn > Column) {
+            throw new ArrayIndexOutOfBoundsException("Column Index Out Of Bound.");
+        }
+        BufferedWriter WriteColumnData = new BufferedWriter(new FileWriter(tbDir, true));
+        for (int i = 0; i < ColumnName.length; i++) {
+            WriteColumnData.write("¤ȸ" + (i+1) + ColumnName[i]);
+        }
+        WriteColumnData.close();
+    }
+
+    public void addRow(String RowName[]) throws IOException {
+        countForRow++;
+        BufferedWriter WriteRowData = new BufferedWriter(new FileWriter(tbDir, true));
+        WriteRowData.newLine();
+        for (int i = 0; i < RowName.length; i++) {
+            if (i > Column) {
+                throw new ArrayIndexOutOfBoundsException("Row Index Out Of Bound.");
+            } else {
+                WriteRowData.write("ȸ" + (i+1) + RowName[i]);
+            }
+        }
+        WriteRowData.close();
     }
 }
